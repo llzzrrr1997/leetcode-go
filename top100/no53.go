d9edd7653a266c187c2603ccc9f7eb25244e7e9f
llzzrrr1997 <llzzrrr1997@gmail.com>
2024-08-12 22:51:39 +0800

    top 100, 普通数组

--- top100/no53.go
+++ top100/no53.go
@@ -1,5 +1,20 @@
 package top100
 
+import "math"
+
 func maxSubArray(nums []int) int {
-	return 0
+	ret := math.MinInt
+	preSum := 0    //当前前缀和
+	minPreSum := 0 //最小前缀和
+	for _, v := range nums {
+		preSum += v
+		tmp := preSum - minPreSum
+		if tmp > ret {
+			ret = tmp
+		}
+		if minPreSum > preSum {
+			minPreSum = preSum
+		}
+	}
+	return ret
 }
